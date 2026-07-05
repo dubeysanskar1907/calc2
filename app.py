91f830ae42fe592267bab6a42cb11928da4e4dbf
dubeysanskar1907 <sanskardubey1907@gmail.com>
2026-07-05 19:01:06 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+# Take two numbers as input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the addition result
+print(f"The sum of {num1} and {num2} is: {result}")
